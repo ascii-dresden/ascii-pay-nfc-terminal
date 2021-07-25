ce61a2a0131a8c05eb5c56a5bf4d7a2c6d3e804f
Lars Westermann <git@lars-westermann.de>
2021-07-25 09:13:51 +0200

    Add install.sh to ci

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-apt update
-apt install libpcsclite1 libpcsclite-dev libnfc-bin libnfc-dev libevdev-dev libevdev-tools libevdev2 libssl-dev
+apt-get install libpcsclite1 libpcsclite-dev libnfc-bin libnfc-dev libevdev-dev libevdev-tools libevdev2 libssl-dev
