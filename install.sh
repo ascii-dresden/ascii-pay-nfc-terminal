7d3847ff34d7539591f2b52a7a36ad9adcffeaed
Lars Westermann <git@lars-westermann.de>
2021-12-05 17:51:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 apt-get install libpcsclite1 libpcsclite-dev libnfc-bin libnfc-dev libevdev-dev \
     libevdev-tools libevdev2 libssl-dev libccid libacsccid1 opensc pcsc-tools \
-    protobuf-compiler libprotobuf-dev protobuf-c-compiler cmake
+    protobuf-compiler libprotobuf-dev protobuf-c-compiler cmake clang libclang-dev
