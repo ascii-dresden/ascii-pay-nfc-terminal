57237c129b5b6f0ab3813e194621642f5a7a0a36
Lars Westermann <git@lars-westermann.de>
2023-04-09 10:25:30 +0200

    Update smartcard_list.txt

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 apt-get install libpcsclite1 libpcsclite-dev libnfc-bin libnfc-dev libevdev-dev \
-    libevdev-tools libevdev2 libssl-dev libccid libacsccid1 opensc pcsc-tools \
-    protobuf-compiler libprotobuf-dev protobuf-c-compiler cmake clang libclang-dev
+    libevdev-tools libevdev2 libccid libacsccid1 opensc pcsc-tools
